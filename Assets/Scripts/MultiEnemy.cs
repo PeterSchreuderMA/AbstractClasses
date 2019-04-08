9e1516e0a923e5f6e0e43a495a40af372865e121
PeterSchreuder_Laptop <24441@ma-web.nl>
2019-04-08 12:18:39 +0200

    Done

---
+++ Assets/Scripts/MultiEnemy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiEnemy : Tower
+{
+    private List<Transform> _target = new List<Transform>();
+
+    protected override void Shoot()
+    {
+        print("Shooting multiple enemies");
+    }
+
+    protected override bool HasTarget()
+    {
+        bool _return = false;
+        // logica voor targetting singleEnemy
+        var cols = Physics.OverlapSphere(transform.position, _radius, _layer);
+
+        if (cols.Length > 0)
+        {
+            _return = true;
+        }
+        
+        return _return;
+    }
+}
---
+++ Assets/Scripts/SingleFriendly.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleFriendly : Tower
+{
+    private Transform _target;
+
+    protected override void Shoot()
+    {
+        print("Shooting single friendly");
+    }
+
+    protected override bool HasTarget()
+    {
+        bool _return = false;
+        // logica voor targetting singleEnemy
+        var cols = Physics.OverlapSphere(transform.position, _radius, _layer);
+
+        if (cols.Length > 0)
+        {
+            _target = cols[0].transform;
+            _return = true;
+        }
+
+        return _return;
+    }
+}
